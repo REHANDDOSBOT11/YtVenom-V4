d2cb6bd63612cbd23e024c287264ef73d6d8ad0d
REHANDDOSBOT11 <khankhalid97462@gmail.com>
2024-08-02 00:04:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6376938455:AAF0O6Je9X5Lxwa5GG7kJ_CaeV-4nZ7esQI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("REHAN_DDOS")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5239197123")
 
 
 
